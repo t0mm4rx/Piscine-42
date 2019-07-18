933ce0fae57e164e900862b0647c00d3d78ca2f9
Tom MARX <tmarx@e2r10p10.42.fr>
2019-07-18 21:44:40 +0200

    correction c10

--- c10/ex02/ft_stdin.c
+++ c10/ex02/ft_stdin.c
@@ -6,7 +6,7 @@
 /*   By: tmarx <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/17 23:02:14 by tmarx             #+#    #+#             */
-/*   Updated: 2019/07/18 21:38:43 by tmarx            ###   ########.fr       */
+/*   Updated: 2019/07/18 21:43:37 by tmarx            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,4 @@ void	ft_display_stdin(int char_count)
 			write(1, &buffer[i], 1);
 		i++;
 	}
-
-}
-
-int		buffer_size(char buffer[900000])
-{
-	int i;
-
-	i = 0;
-	while (buffer[i++]);
-	return (i);
 }
