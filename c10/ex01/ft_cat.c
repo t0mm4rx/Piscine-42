e8e6da007ed7a56f7feda35bbf582aad829dd984
Tom MARX <tmarx@e2r10p10.42.fr>
2019-07-18 21:42:14 +0200

    eofkoefkef

--- c10/ex01/ft_cat.c
+++ c10/ex01/ft_cat.c
@@ -6,12 +6,13 @@
 /*   By: tmarx <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/16 14:43:28 by tmarx             #+#    #+#             */
-/*   Updated: 2019/07/18 17:20:20 by tmarx            ###   ########.fr       */
+/*   Updated: 2019/07/18 19:02:12 by tmarx            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <fcntl.h>
+#include <libgen.h>
 
 int		ft_strlen(char *str)
 {
@@ -28,7 +29,7 @@ void	ft_putstr(int chan, char *str)
 	write(chan, str, ft_strlen(str));
 }
 
-void	ft_display_file(char *file)
+void	ft_display_file(char *file, char *program_name)
 {
 	int		fd;
 	int		n;
@@ -37,7 +38,8 @@ void	ft_display_file(char *file)
 	fd = open(file, 0);
 	if (fd == -1)
 	{
-		ft_putstr(2, "ft_cat: ");
+		ft_putstr(2, program_name);
+		ft_putstr(2, ": ");
 		ft_putstr(2, file);
 		ft_putstr(2, ": No such file or directory\n");
 		return ;
@@ -69,7 +71,7 @@ int		main(int argc, char **argv)
 	{
 		while (i < argc)
 		{
-			ft_display_file(argv[i++]);
+			ft_display_file(argv[i++], basename(argv[0]));
 		}
 	}
 	else
--- c10/ex02/ft_stdin.c
+++ c10/ex02/ft_stdin.c
@@ -6,7 +6,7 @@
 /*   By: tmarx <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/17 23:02:14 by tmarx             #+#    #+#             */
-/*   Updated: 2019/07/17 23:41:41 by tmarx            ###   ########.fr       */
+/*   Updated: 2019/07/18 21:38:43 by tmarx            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,29 @@
 void	ft_display_stdin(int char_count)
 {
 	int		n;
-	char	buffer[32000];
+	char	buffer2[1];
+	char	buffer[900000];
 	int		i;
+	int		j;
 
 	i = 0;
-	n = read(0, buffer, 32000);
-	while (i < n)
+	j = 0;
+	while ((n = read(0, buffer2, 1)) > 0)
+		buffer[j++] = buffer2[0];
+	while (i < j)
 	{
-		if (i >= n - char_count)
+		if (i >= j - char_count)
 			write(1, &buffer[i], 1);
 		i++;
 	}
+
+}
+
+int		buffer_size(char buffer[900000])
+{
+	int i;
+
+	i = 0;
+	while (buffer[i++]);
+	return (i);
 }
